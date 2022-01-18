eb524d8222171952abf60cb41e33f5b1096fb2f8
Chaz Simons <chaz.simons@gmail.com>
2022-01-18 09:59:56 -0800

    Add test for number of results returned

--- app/facades/books_facade.rb
+++ app/facades/books_facade.rb
@@ -2,9 +2,11 @@ class BooksFacade
 
   def self.get_books_about(location, quantity)
     books = BooksService.get_books_about(location, quantity)
-    total_books_found = books[:numFound]
-    books[:docs].map do |book|
+    books_hash = {}
+    books_hash[:books] = books[:docs].map do |book|
       Book.new(book)
     end
+    books_hash[:total_books_found] = books[:numFound]
+    books_hash
   end
 end

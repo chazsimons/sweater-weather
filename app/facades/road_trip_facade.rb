f5fdb0b17149e3d51b9a21b9dd1a8329f2304b8d
Chaz Simons <chaz.simons@gmail.com>
2022-01-18 08:01:13 -0800

    Minor code quality fix.

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -23,9 +23,9 @@ class RoadTripFacade
       if trip.time_in_hrs != 1000
         weather_at_eta[:temp_at_eta] = weather_at_time[:temp]
         weather_at_eta[:conditions]  = weather_at_time[:weather][0][:description]
-        trip_hash[:weather_at_eta] = weather_at_eta
+        trip_hash[:weather_at_eta]   = weather_at_eta
       else
-        trip_hash[:weather_at_eta] = weather_at_eta
+        trip_hash[:weather_at_eta]   = weather_at_eta
       end
       trip_hash
     # end

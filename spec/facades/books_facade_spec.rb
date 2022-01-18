d6bfce1024e12415dbb524e2d278e43284838d93
Chaz Simons <chaz.simons@gmail.com>
2022-01-18 08:54:55 -0800

    Start books facade proper

---
+++ spec/facades/books_facade_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe BooksFacade do
+  it 'returns the correct attributes of searched books', :vcr do
+    books = BooksFacade.get_books_about('denver,co', 5)
+
+    expect(books.first).to be_a(Book)
+  end
+end

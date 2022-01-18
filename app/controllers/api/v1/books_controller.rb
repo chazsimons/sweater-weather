f27122ddafea7a1a186b2596e9fb2f94341abf38
Chaz Simons <chaz.simons@gmail.com>
2022-01-18 08:30:35 -0800

    Add spec and method for books facade connection

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BooksController < ApplicationController
 
   def index
-
+    # require "pry"; binding.pry
+    BooksFacade.get_books_about(params[:location], params[:quantity])
   end
 end
